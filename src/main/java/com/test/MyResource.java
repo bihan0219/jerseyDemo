62627745e9465cec371903cc0d887c871594626f
高帅 <gaoshuai@gaoshuaideMacBook-Air.local>
2017-08-05 17:52:48 +0800

    test

--- src/main/java/com/test/MyResource.java
+++ src/main/java/com/test/MyResource.java
@@ -11,7 +11,7 @@ public class MyResource {
 	@GET
 	  @Produces(MediaType.TEXT_PLAIN)
 	  public String sayPlainTextHello() {
-	    return "Hello Jersey demo";
+	    return "Hello Jersey";
 	  }
 
 	  // This method is called if XML is request
